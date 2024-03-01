6dcd0c2d7e47aa6e2f7f439e8db299aa95f9c847
Rodri <rodrigoalvesdemacedo@live.com>
2024-03-01 03:08:13 +0100

    pokemonCard almost done

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -1,9 +1,22 @@
+import { NavLink } from "react-router-dom";
+import useFetch from "../../Hooks/useFetch";
 import "./PokemonCard.scss";
 
 const PokemonCard = ({ pokemon }) => {
+  const [pokemonData] = useFetch(pokemon.url);
   return (
     <div className="pokemon-card">
-      <p className="pokemon-card__name">{pokemon.name}</p>
+      <img className="pokemon-card__img" src={pokemonData?.sprites?.other?.["official-artwork"]?.front_default} />
+      <p className="pokemon-card__number">#{pokemonData?.id || ""}</p>
+      <p className="pokemon-card__name">{pokemonData?.name || "- -"}</p>
+      <div className="pokemon-card__data">
+        <span className="pokemon-card__attr">WEIGHT</span> {pokemonData?.weight}KG
+        <span className="pokemon-card__attr">HEIGHT</span> {pokemonData?.height}M
+      </div>
+
+      <NavLink to={`/pokemon/${pokemonData?.id}`}>
+        <button className="btn pokemon-card-more-info">+INFO</button>
+      </NavLink>
     </div>
   );
 };
--- src/pages/PokemonsPage/PokemonsPage.js
+++ src/pages/PokemonsPage/PokemonsPage.js
@@ -3,18 +3,27 @@ import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import "./PokemonsPage.scss";
 import PokemonCard from "../../components/PokemonCard/PokemonCard";
+import { useState } from "react";
 
-const API_URL = `${process.env.REACT_APP_API_URL}/pokemon?offset=20&limit=2000`;
+const API_URL = `${process.env.REACT_APP_API_URL}/pokemon?offset=0&limit=2000`;
+const NUM_ITEMS_PER_PAGE = 9;
 
 const PokemonsPage = () => {
   const [pokemonsData] = useFetch(API_URL);
+  const [numPokemonsToShow, setNumPokemonsToShow] = useState(NUM_ITEMS_PER_PAGE);
+  const firstPokemons = pokemonsData?.results?.slice(0, numPokemonsToShow) || [];
+  const showMorePokemons = () => setNumPokemonsToShow(numPokemonsToShow + NUM_ITEMS_PER_PAGE);
 
   return (
     <div className="pokemons-page page">
       <Header></Header>
 
-      <div className="page__content">
-        <div className="pokemons-page__pokemon-list">{pokemonsData?.results?.map((pokemon) => <PokemonCard key={pokemon.name} pokemon={pokemon}></PokemonCard>)}</div>
+      <div className="page__content pokemons-page__content">
+        <div className="pokemons-page__pokemon-list">
+          {firstPokemons.map((pokemon) => <PokemonCard key={pokemon.name} pokemon={pokemon}></PokemonCard>)}
+        </div>
+
+        <button onClick={showMorePokemons} className="page__show-more btn">+ MORE</button>
       </div>
 
       <Footer></Footer>
